196c40d25ee45e9f63e76986b27ee09b17f612f7
Ha Huu Nhut <ha.huu.nhut@sun-asterisk.com>
2021-06-24 09:19:02 +0700

    Add Home Page and get list category from db

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
-  belongs_to :parent, class_name: Category.name
+  belongs_to :parent, class_name: Category.name, optional: true
   has_many :childrens, class_name: Category.name, foreign_key: :parent_id,
                       dependent: :destroy
+
+  scope :parents, ->{where("parent_id is null")}
+
+  accepts_nested_attributes_for :childrens
 end
