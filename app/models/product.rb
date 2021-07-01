ed6d090dd519561fc6b089aedddc2db9d5099538
Ha Huu Nhut <ha.huu.nhut@sun-asterisk.com>
2021-07-01 22:30:58 +0700

    Implement admin view product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :category
   has_many :order_details, dependent: :destroy
   has_many :evaluates, dependent: :destroy
+
+  scope :sort_update, ->{order(updated_at: :desc)}
 end
